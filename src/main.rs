1004c5c17c88868531ffece1ad0da69b0e5cf148
John Allison <john123allison@gmail.com>
2020-09-06 21:46:58 -0400

    Super basic prototype; lists the given directory and will probably panic if anything else is tried.

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,23 @@ fn main() {
     let dir_entries = fs::read_dir(dir).expect("Could not read directory.");
 
     // test
-    default_display(dir_entries);
+    default_display(dir_entries, false);
 }
 
 /// The default display that displays each entry of the directory line by line non-recursively.
-fn default_display(dir: fs::ReadDir) {
+fn default_display(dir: fs::ReadDir, show_hidden: bool) {
     for entry in dir {
-        println!("{:?}", entry);
+        // unwrap entry to a result
+        let entry_result = entry.expect("Cannot read entry.");
+        let file_str = entry_result.file_name().to_str().unwrap().to_string();
+
+        // determine if it should be shown if a dotfile
+        if show_hidden {
+            println!("{:?}", &file_str);
+        } else {
+            if !&file_str.starts_with('.') {
+                println!("{:?}", file_str);
+            }
+        }
     }
 }
